606157931997fd6a27d90235a35ad176b5096767
Yupeeeeeeeee <bodo9545@gmail.com>
2026-08-03 20:57:47 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,19 +4,18 @@ let result = document.getElementById("result");
 let error = document.getElementById("error");
 button.addEventListener("click",function(){
     let birthYear= birthYearInp.value;
-    let age = 2026 - birthYear ;
     if(birthYear === ""){
         error.textContent = "Please Enter Your Birth Year"
-        result.textContent = ""
         return;
     }
+    let age = 2026 - birthYear;
     if (age >= 100){
-        result.textContent = "Bro you must be dead LOL :)"
+        error.textContent = "The entered birth year seems invalid"
     }
     else if (age < 100 && age > 0){
-    result.textContent = "You Age is" + "age" ;
+    result.textContent = "You Age is " + age ;
     }
     else if (age <= 0){
-        result.textContent= "How did you exist then , huh ???"
+        error.textContent= "Please enter a valid birth year"
     }
 })
