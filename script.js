b9bd5d175a2dc8a646d369de75efc9da5f29bdf7
Yupeeeeeeeee <bodo9545@gmail.com>
2026-08-03 19:02:39 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ button.addEventListener("click",function(){
     let age = 2026 - birthYear ;
     if(birthYear === ""){
         error.textContent = "Please Enter Your Birth Year"
+        result.textContent = ""
     }
     if (age >= 100){
         result.textContent = "Bro you must be dead LOL :)"
@@ -14,7 +15,7 @@ button.addEventListener("click",function(){
     else if (age < 100 && age > 0){
     result.textContent = "You Age is" + "age" ;
     }
-    else if (age < 0){
+    else if (age <= 0){
         result.textContent= "How did you exist then , huh ???"
     }
 })
