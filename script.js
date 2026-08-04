db483495ebf3ef453ee31b72f6d3f887b35194c5
Yupeeeeeeeee <bodo9545@gmail.com>
2026-08-04 19:55:05 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let title =document.getElementById("title");
+let taskInp = document.getElementById("taskInp");
+let button = document.getElementById("addBtn");
+let message = dochment.getElementById("message");
+let taskCount = document.getElementById("taskCount");
+let taskList = document.getElementById("taskList");
