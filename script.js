2d402ca0af755fee37157403cbc2f02236907e4c
Yupeeeeeeeee <bodo9545@gmail.com>
2026-08-06 14:50:08 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 let title =document.getElementById("title");
 let taskInp = document.getElementById("taskInp");
-let button = document.getElementById("addBtn");
-let message = dochment.getElementById("message");
+let addBtn = document.getElementById("addBtn");
+let message = document.getElementById("message");
 let taskCount = document.getElementById("taskCount");
 let taskList = document.getElementById("taskList");
 
 let count= 0;
 
 addBtn.addEventListener("click",function(){
-
-   count++;
-    taskCount.textContent= count;
     let taskText = taskInp.value;
-message.textContent= taskText;
+    message.textContent="New Task : " + taskText;
+    count++;
+    taskCount.textContent= count;
+
+    taskInp.value = "";
 });
